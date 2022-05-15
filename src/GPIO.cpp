e84ef807aa18d1337a5cea18dd87604fe6c3b009
juamarCas <juanmartincas@gmail.com>
2022-05-15 22:55:56 +0100

    fixed toggle GPIO

--- src/GPIO.cpp
+++ src/GPIO.cpp
@@ -13,9 +13,11 @@ bool GPIO::GPIO_Toggle() {
     std::string _res;
     if (_currentState == "1") _res = "0";
     else if (_currentState == "0") _res = "1";
+    
 
     if (write(_file, _res.c_str(), 1) != 1) {
         std::cout << "error" << std::endl;
         return false;
     }
+    _currentState = _res;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -17,26 +17,38 @@ int main(int argv, const char ** argc){
 
     
     const std::string msg      = "Hello from PI!";
-    const std::string topic    = "testtopic";
+    const std::string topic    = "Room1/RPI";
 
     MQTTClient mqttClient (host, port, protocol);
     mqttClient.Connect("sssss");
     mqttClient.PublishToTopic(msg, topic);
     mqttClient.SubscribeToTopic(topic);
 
+    GPIO _yellow("23", "out");
+    GPIO _green("24", "out");
+    GPIO _red("25", "out");
+
+
     std::thread mqtt_client_thread(
-        [&mqttClient](){
-            mqttClient.StartListening([](const std::string& payload){
+        [&mqttClient, &_red, &_yellow, &_green](){
+            mqttClient.StartListening([&_red, &_yellow, &_green](const std::string& payload){
                 std::cout<<payload<<std::endl;
+                if (payload == "LED1") {
+                    _yellow.GPIO_Toggle();
+                }
+                else if (payload == "LED2") {
+                    _green.GPIO_Toggle();
+                }
+                else if (payload == "LED3") {
+                    _red.GPIO_Toggle();
+                }
             });
         }
     );
 
     mqtt_client_thread.detach();
 
-    GPIO _gpio("23", "out");
-    _gpio.GPIO_Write("1");
-    _gpio.GPIO_Toggle();
+    
   
 
     while(1){
